cb4033fa3b2d3f1b0ecf40f6aa414dfc7204e2e8
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-17 15:05:16 +0530

    [Tejaswini] Add 'Add Contact' Program

---
+++ Address_Book/Address_Book/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Specialized;
+using NLog;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //AddContact[] addContact =new AddContact[4];
+            int number = 0;
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.getContactDetails();
+            Console.Write("Enter Number");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                addressBookMain.addContactDetails();
+            }
+            //addressBookMain.getContactDetails();
+            addressBookMain.showContactDetails();
+        }
+    }
+}
